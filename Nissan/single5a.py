6fe2c31c01e1e950db770b2f19928769b8e942e1
nate <narget@umich.edu>
2018-02-27 00:18:35 +0000

    singles

--- Nissan/single5a.py
+++ Nissan/single5a.py
@@ -54,7 +54,7 @@ cap = MPR121.MPR121()
 
 # Initialize communication with MPR121 using default I2C bus of device, and
 # default I2C address (0x5A).  On BeagleBone Black will default to I2C bus 0.
-if not cap.begin(address=args.slave):
+if not cap.begin(address=i2caddress):
     print('Error initializing MPR121.  Check your wiring!')
     sys.exit(1)
 
